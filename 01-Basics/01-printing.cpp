b492406801034a7f62a4ca006fb7b60abf23dd3d
Ahmed Waheedy <ahmdwaheedy@gmail.com>
2025-12-27 14:07:47 +0200

    Added 01-printing: First task to practice std::cout syntax

---
+++ 01-Basics/01-printing.cpp
@@ -0,0 +1,11 @@
+/*
+ * Description: First program to practice output syntax.
+ * Concept: std::cout, endl
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "I am learning C++ :)" << std::endl;
+    return 0;
+}
